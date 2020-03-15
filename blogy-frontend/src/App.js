73cb87ecea03acc2c7ca557554ec50ee3a6abf1e
Renad-Ahmad <rena.34end@gmail.com>
2020-03-15 21:14:20 +0300

    Add Articles Component

---
+++ blogy-frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Articles from './articles/components/articles'; 
+import './App.css';
+
+class App extends React.Component {
+
+  render() {
+    return ( 
+      <div className="App"> 
+      <header className="App-header">
+      <p> 
+      Welcome to Blogy! 
+      </p> 
+      </header> 
+      <Articles /> 
+      </div> 
+    );
+  } 
+}
+
+export default App;
